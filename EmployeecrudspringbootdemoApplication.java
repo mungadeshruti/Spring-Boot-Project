81b175cddd40bbf4473a580b32e8cbb4d4e0a80a
mungadeshruti <161950919+mungadeshruti@users.noreply.github.com>
2024-07-18 11:39:54 +0530

    Add files via upload

---
+++ EmployeecrudspringbootdemoApplication.java
@@ -0,0 +1,16 @@
+package com.employecrudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeecrudspringbootdemoApplication
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(EmployeecrudspringbootdemoApplication.class, args);
+	}
+
+}
+
